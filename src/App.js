eee9592a6f40ceb0aba1b8cbf3d9961150feb5b0
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-01-19 21:56:14 +0300

    refactor: xios.get request to API >> PostService.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
-import axios from 'axios'; 
 import { useEffect } from "react";
+import PostService from "./API/PostService";
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -28,8 +28,8 @@ function App() {
   }
 
   async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    setPosts(response.data);
+    const posts = await PostService.getAll()
+    setPosts(posts);
   }
 
   const removePost = (post) => {
